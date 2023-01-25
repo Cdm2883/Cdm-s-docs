dd2215ae31dde581195b7b8d6e37bedeffd4362b
Cdm2883 <96967341+Cdm2883@users.noreply.github.com>
2023-01-25 17:12:47 +0800

    Add files via upload

---
+++ data/module/blog-mode.js
@@ -0,0 +1,7 @@
+/* 
+    博客模式扩展
+ */
+
+var $ = mdui.$;
+
+debugTool.loaded();
---
+++ data/module/saveManager-one.js
@@ -0,0 +1,11 @@
+/* 
+    saveManager - only one server
+*/
+
+var $ = mdui.$;
+
+$('#page-more-menu-dialog .change-servers').remove();
+
+var saveManager = {};
+
+debugTool.loaded();
