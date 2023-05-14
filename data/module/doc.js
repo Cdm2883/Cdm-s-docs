f91e806a09be25382cdf347007b2fbb8d461ee40
Cdm2883 <Cdm2883@hotmail.com>
2023-05-14 21:51:55 +0800

    Add files via upload

--- data/module/doc.js
+++ data/module/doc.js
@@ -20,9 +20,21 @@ var doc = {
             tabManager.append(path);
         }
     },
-    //
     ff: (path) => {
         let mParent = fileManager.path;
+        path = path.replace(/%20/g, ' ');
+        if (path.substring(0, 1) == '/') path = mParent + path;
+        if (path.substring(0, 2) == './') path = path.slice(2);
+        // if (path.substring(0, 10) == '../../docs') path = path.slice(10);
+        let mGetDir = fileAPI.getDir(path);
+        if (mGetDir.length !== 0) {
+            fileManager.toPath(path);
+            fileManager.drawer.open();
+        } else {
+            tabManager.append(path);
+        }
+    },
+    fp: (path, mParent) => {
         path = path.replace(/%20/g, ' ');
         if (path.substring(0, 1) == '/') path = mParent + path;
         if (path.substring(0, 2) == './') path = path.slice(2);
