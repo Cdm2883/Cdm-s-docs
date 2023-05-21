e201d57f2e05f0bb8660f5c0491ebf72b0e8bd92
Cdm2883 <Cdm2883@hotmail.com>
2023-05-21 15:03:23 +0800

    Add files via upload

--- data/module/doc.js
+++ data/module/doc.js
@@ -6,12 +6,31 @@ var $ = mdui.$;
 
 var doc = {
     t: btnEvent.toNodeByID,
-    f: (path) => {
+    f_: (path) => {
         let mParent = fileAPI.parentPath(tabManager.items[tabManager.nowIndex].path);
         path = path.replace(/%20/g, ' ');
-        if (path.substring(0, 2) == './') path = '/' + mParent + path.slice(1);
-        if (path.substring(0, 1) == '/') path = path;
+        // if (path.substring(0, 2) == './') path = '/' + mParent + path.slice(1);
+        if (path.substring(0, 1) == '/') {
+            path = path;
+        } else {
+            if (path.substring(0, 2) == './') path = path.slice(2);
+            path = mParent + '/' + path;
+        }
         // if (path.substring(0, 10) == '../../docs') path = path.slice(10);
+        let mGetDir = fileAPI.getDir(path);
+        if (mGetDir.length !== 0) {
+            fileManager.toPath(path);
+            fileManager.drawer.open();
+        } else {
+            tabManager.append(path);
+        }
+    },
+    f: (path) => {
+        path = fileManager.getPagePath(
+            fileAPI.parentPath(tabManager.items[tabManager.nowIndex].path),
+            path
+        );
+
         let mGetDir = fileAPI.getDir(path);
         if (mGetDir.length !== 0) {
             fileManager.toPath(path);
